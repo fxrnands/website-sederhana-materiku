e0b641e13f56fde578acd382a32d5e96fde9a0ed
fxrnands <fxrnands@gmail.com>
2022-08-17 02:53:26 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,18 @@
+const nama = "muhammad fernanda kholil jibron"
+let usia = 30;
+
+
+function generateBiodata() {
+    if (usia == 30) {
+        // INI ADALAH KONDISI PERTAMA
+        console.log ('anda dewasa')
+    } else {
+        // INI ADALAH JIKA KONDISI TIDAK TERPENUHI
+        console.log ('anda anak anak')
+    }
+}
+
+console.log(nama);
+console.log(usia);
+
+generateBiodata();
